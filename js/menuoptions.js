f6cf5325ce298afc15cce99127ada6d1b76b06cc
Vishal Mishra <vishalmishra3141005@gmail.com>
2023-03-24 22:55:37 +0530

    Added styling and js structure

---
+++ js/menuoptions.js
@@ -0,0 +1,32 @@
+const menuoptions = [
+    {
+        "name": "Model S"
+    },
+    {
+        "name": "Model 3"
+    },
+    {
+        "name": "Model X"
+    },
+    {
+        "name": "Model Y"
+    },
+    {
+        "name": "Solar Roof"
+    },
+    {
+        "name": "Solar Panels"
+    },
+    {
+        "name": "Powerwall"
+    },
+    {
+        "name": "Shop"
+    },
+    {
+        "name": "Account"
+    },
+    {
+        "name": "Menu"
+    }
+];
